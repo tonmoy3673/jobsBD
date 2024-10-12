2f60cdfcda7e6ab2458805a684dcdf1224779ed9
tonmoy <tonmoy3673@gmail.com>
2024-10-12 19:26:28 +0600

    :tada: responsive navbar

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -5,11 +5,13 @@ import NavBar from "../Shared/Navbar";
 
 const Main = () => {
     return (
-        <div>
+     
+        <div className="px-8 md:px-20 py-4 md:py-8">
         <NavBar/>
         <Outlet/>
         <Footer/>
         </div>
+        
     );
 };
 
--- src/Components/Shared/Navbar.jsx
+++ src/Components/Shared/Navbar.jsx
@@ -1,8 +1,47 @@
+import { NavLink } from "react-router-dom";
+
 const NavBar=()=>{
+    const links=<>
+    <li><NavLink>Statistics</NavLink></li>
+    <li><NavLink>Applied Jobs</NavLink></li>
+    <li><NavLink>Blog</NavLink></li>
+    </>
     return (
-        <div>
-            <h2>This is NavBar</h2>
-        </div>
+
+        <div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h8m-8 6h16" />
+        </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+       {links}
+      </ul>
+    </div>
+    <a className="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className=" menu-horizontal px-1">
+      {links}
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <a className="btn">Button</a>
+  </div>
+</div>
     )
 };
 
