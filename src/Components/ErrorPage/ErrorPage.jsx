b740a3d06daf2ce4ae55517422cbba4e0a0d41dc
tonmoy <tonmoy3673@gmail.com>
2024-10-15 23:00:00 +0600

    :bulb: display error status code in error page

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -7,6 +7,11 @@ const ErrorPage=()=>{
             <h2 className="text-lg lg:text-2xl font-semibold text-blue-700 py-2">Oops!</h2>
             <h4 className="font-semibold text-red-700 mb-2  text-lg lg:text-xl">An unexpected Error has been Occurred.</h4>
             <div className="py-2">
+                {
+                    error.status && (
+                        <span className="text-red-900 font-bold">{error.status}</span>
+                    )
+                }
                 <p className="text-red-800 font-semibold">{error.statusText || error.message}</p>
             </div>
             <Link to='/home' className="underline text-blue-800">Go Back to Home Page</Link>
