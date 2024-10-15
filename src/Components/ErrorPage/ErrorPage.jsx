2ce201cce09e74cffa99772c141cbac8aa83e862
tonmoy <tonmoy3673@gmail.com>
2024-10-15 22:53:44 +0600

    :sparkles: integrated with errorPage

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -3,13 +3,13 @@ import { Link, useRouteError } from "react-router-dom";
 const ErrorPage=()=>{
     const error=useRouteError();
     return (
-        <div>
-            <h2 className="text-lg lg:text-xl font-semibold text-blue-700 py-2">Oops!</h2>
+        <div className="text-center py-4 md:py-8">
+            <h2 className="text-lg lg:text-2xl font-semibold text-blue-700 py-2">Oops!</h2>
             <h4 className="font-semibold text-red-700 mb-2  text-lg lg:text-xl">An unexpected Error has been Occurred.</h4>
             <div className="py-2">
-                <p className="text-red-800">{error.statusText || error.message}</p>
+                <p className="text-red-800 font-semibold">{error.statusText || error.message}</p>
             </div>
-            <Link to='/home'>Go Back to Home Page</Link>
+            <Link to='/home' className="underline text-blue-800">Go Back to Home Page</Link>
         </div>
     )
 };
--- src/Components/Router/router.jsx
+++ src/Components/Router/router.jsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "../ErrorPage/ErrorPage";
 import Home from "../Home/Home";
 import Main from "../Main/Main";
 
@@ -6,6 +7,7 @@ export const router= createBrowserRouter([
     {
         path:'/',
         element:<Main/>,
+        errorElement:<ErrorPage/>,
         children:[
             {
                 path:'/',
