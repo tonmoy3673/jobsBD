20fee2ad99cda3e5222d3a3c7dfffdf5d0aa9876
tonmoy <tonmoy3673@gmail.com>
2024-10-16 22:11:17 +0600

    :bulb: set banner content

--- src/Components/Home/Banner.jsx
+++ src/Components/Home/Banner.jsx
@@ -2,20 +2,20 @@
  import user from '/public/Images/users/user.png';
 const Banner=()=>{
     return (
-        <div className="banner-container ">
+        <div className="banner-container lg:flex items-center justify-between">
            <div>
            <h1>One Step <br/> Closer To Your <br/>
             <span>Dream Job</span>
             </h1>
-            <p>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+            <p className='w-full lg:w-[550px]'>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
             <div>
-                <button className="common-btn bg-[#2596be] text-white"> Get Stared</button>
+                <button className="common-btn hover:bg-amber-500"> Get Stared</button>
             </div>
            </div>
 
            {/* ============= Image Part ======== */}
            <div>
-            <img src={user} className='lg:w-[450px] md:h-[350px] ' alt='user'/>
+            <img src={user} className='lg:w-[400px] md:h-[350px] ' alt='user'/>
            </div>
         </div>
     )
