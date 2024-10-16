f86b0673fadb4945b1555d6650101f592c7067de
tonmoy <tonmoy3673@gmail.com>
2024-10-17 00:33:43 +0600

    :tada: completed categories list section

--- src/Components/Home/Categories.jsx
+++ src/Components/Home/Categories.jsx
@@ -13,7 +13,7 @@ console.log(categories)
     return (
         <div className="py-8 lg:py-16">
             <h2 className="text-xl lg:text-3xl font-semibold text-center py-3">Job Category List</h2>
-            <div>
+            <div className=" grid grid-cols-2 lg:grid-cols-4 py-6 lg:py-12 gap-10 ">
                 {
                     categories && categories.length>0?(
                         categories.map((category)=><Category category={category} key={category.id}></Category>)
--- src/Components/Home/Category.jsx
+++ src/Components/Home/Category.jsx
@@ -3,13 +3,13 @@ const Category = ({category}) => {
     const {logo,category_name,availability}=category
     console.log(category);
     return (
-        <div>
-            <div>
-                <img src={logo}/>
+        <div className="bg-sky-100 p-4 rounded-lg">
+            <div className="mt-1 lg:mt-2">
+                <img src={logo} className="mx-auto bg-sky-50 p-1"/>
             </div>
-            <div>
-                <h4>{category_name}</h4>
-                <p>{availability}</p>
+            <div className="py-2 text-center">
+                <h4 className="py-1 text-base lg:text-lg font-semibold">{category_name}</h4>
+                <p className="py-1">{availability}</p>
             </div>
         </div>
     );
