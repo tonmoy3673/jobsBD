b8f772d1dbc0dae7a0f12069670e5026a8e4085d
tonmoy <tonmoy3673@gmail.com>
2024-10-17 01:30:00 +0600

    :bulb: display all jobs

--- src/Components/Home/AllJobs.jsx
+++ src/Components/Home/AllJobs.jsx
@@ -10,12 +10,12 @@ const AllJobs=()=>{
     },[])
     
     return (
-        <div>
-            <div className="py-8 lg:py-12 text-center">
+        <div  className="py-8 lg:py-12 text-center">
+            <div>
                 <h2 className="text-xl lg:text-3xl font-semibold text-center py-3">Featured Jobs</h2>
                 <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
-            <div>
+            <div className="grid grid-cols-2 lg:grid-cols-3 py-2 lg:py-8 mt-2 lg:mt-6 gap-4 lg:gap-8 items-center justify-center">
             {
                 jobs && jobs.length>0 ? (
                     jobs.map((job)=><Job job={job} key={job.id}/>)
--- src/Components/Home/Job.jsx
+++ src/Components/Home/Job.jsx
@@ -4,9 +4,25 @@ const Job = ({job}) => {
     const {location,remote_or_onsite,company_name,job_title,job_type,salary,logo}=job
     console.log(job);
     return (
-        <div>
+        <div className="">
             <div>
-                <img src={logo} alt="logo"/>
+                <img src={logo} alt="logo" className="w-[50px] lg:w-[100px] mx-auto"/>
+            </div>
+            <h4>{job_title}</h4>
+            <p>{company_name}</p>
+            <div className="flex gap-2 items-center justify-center">
+                <button>{remote_or_onsite}</button>
+                <button>{job_type}</button>
+            </div>
+            <div>
+            <div>
+                {/* ======== img= */}
+            <p>{location}</p>
+            </div>
+            <div>
+                {/* ====== img ========= */}
+                <p>{salary}</p>
+            </div>
             </div>
         </div>
     );
