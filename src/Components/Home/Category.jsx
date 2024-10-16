63c76b5e5243de277efe2b6ef3cbbe11f93fc438
tonmoy <tonmoy3673@gmail.com>
2024-10-17 00:19:54 +0600

    :bulb: added icons

--- src/Components/Home/Category.jsx
+++ src/Components/Home/Category.jsx
@@ -1,9 +1,16 @@
 
 const Category = ({category}) => {
+    const {logo,category_name,availability}=category
     console.log(category);
     return (
         <div>
-            
+            <div>
+                <img src={logo}/>
+            </div>
+            <div>
+                <h4>{category_name}</h4>
+                <p>{availability}</p>
+            </div>
         </div>
     );
 };
