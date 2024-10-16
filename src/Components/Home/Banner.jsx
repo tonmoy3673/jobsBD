11edd93e230edf944e5b95ea3cba081caea1b972
tonmoy <tonmoy3673@gmail.com>
2024-10-16 22:01:36 +0600

    :tada: added banner images and set height width

--- src/Components/Home/Banner.jsx
+++ src/Components/Home/Banner.jsx
@@ -1,9 +1,22 @@
+
+ import user from '/public/Images/users/user.png';
 const Banner=()=>{
     return (
-        <div>
-            <h1>One Step <br/> Closer To Your <br/>
+        <div className="banner-container ">
+           <div>
+           <h1>One Step <br/> Closer To Your <br/>
             <span>Dream Job</span>
-            </h1>  
+            </h1>
+            <p>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+            <div>
+                <button className="common-btn bg-[#2596be] text-white"> Get Stared</button>
+            </div>
+           </div>
+
+           {/* ============= Image Part ======== */}
+           <div>
+            <img src={user} className='lg:w-[450px] md:h-[350px] ' alt='user'/>
+           </div>
         </div>
     )
 };
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,9 @@
+import Banner from "./Banner";
+
 const Home=()=>{
    return <div>
-    <h2>This is Home Page</h2>
+    {/* ============ Banner Section ============= */}
+    <Banner/>
    </div> 
 };
 
