91971486774092e7681c7a4e3df7507be8ed780b
tonmoy <tonmoy3673@gmail.com>
2024-10-16 21:38:45 +0600

    :bulb: create banner section

---
+++ src/Components/Home/Banner.jsx
@@ -0,0 +1,11 @@
+const Banner=()=>{
+    return (
+        <div>
+            <h1>One Step <br/> Closer To Your <br/>
+            <span>Dream Job</span>
+            </h1>  
+        </div>
+    )
+};
+
+export default Banner;
