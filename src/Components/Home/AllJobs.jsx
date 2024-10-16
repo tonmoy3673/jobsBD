bf26a9aaec2ae52b296f754d0778d7d39a526086
tonmoy <tonmoy3673@gmail.com>
2024-10-17 00:37:04 +0600

    :bulb: created all jobs section

---
+++ src/Components/Home/AllJobs.jsx
@@ -0,0 +1,14 @@
+const AllJobs=()=>{
+    return (
+        <div>
+            <div>
+                <h2>Featured Jobs</h2>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
+};
+
+export default AllJobs;
