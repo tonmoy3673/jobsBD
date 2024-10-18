0b74b6cb84edf8f4724a57b6a7ba3250dea4cda1
tonmoy <tonmoy3673@gmail.com>
2024-10-18 20:08:12 +0600

    :truck: create applied jobs file and set route

---
+++ src/Components/Pages/AppliedJobs.jsx
@@ -0,0 +1,11 @@
+
+
+const AppliedJobs = () => {
+    return (
+        <div>
+            <h2>Applied Jobs</h2>
+        </div>
+    );
+};
+
+export default AppliedJobs;
--- src/Components/Router/router.jsx
+++ src/Components/Router/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "../ErrorPage/ErrorPage";
 import Home from "../Home/Home";
 import Main from "../Main/Main";
+import AppliedJobs from "../Pages/AppliedJobs";
 import JobDetails from "../Pages/JobDetails";
 
 export const router= createBrowserRouter([
@@ -22,6 +23,11 @@ export const router= createBrowserRouter([
                 path:'/job/:id',
                 element:<JobDetails/>,
                 loader:()=> fetch ('/data/jobs.json')
+            },
+            {
+                path:'/applied',
+                element:<AppliedJobs/>,
+                loader:()=>fetch('/data/jobs.json')
             }
         ]
     },
