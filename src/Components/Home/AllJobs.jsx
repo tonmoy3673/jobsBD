150f5b379bddd372111d400213e769c68d6aef9b
tonmoy <tonmoy3673@gmail.com>
2024-10-18 15:32:39 +0600

    :bulb: create show all button

--- src/Components/Home/AllJobs.jsx
+++ src/Components/Home/AllJobs.jsx
@@ -12,7 +12,7 @@ const AllJobs=()=>{
     return (
         <div  className="py-8 lg:py-12 text-center">
             <div>
-                <h2 className="text-xl lg:text-3xl font-semibold text-center py-3">Featured Jobs</h2>
+                <h2 className="text-xl lg:text-3xl font-semibold text-center mb-3">Featured Jobs</h2>
                 <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
             <div className="grid grid-cols-2 lg:grid-cols-3 py-2 lg:py-8 mt-2 lg:mt-6 gap-4 lg:gap-8 items-center justify-center">
@@ -23,6 +23,10 @@ const AllJobs=()=>{
                     <p>No Jobs Available</p>
                 )
             }
+            </div>
+            {/* ========== show all Button ============ */}
+            <div>
+                <button className="common-btn hover:bg-amber-500 py-[5px] lg:py-[10px] px-2 lg:px-[15px]">Show All</button>
             </div>
         </div>
     )
