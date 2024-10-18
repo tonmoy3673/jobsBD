7f74bf6f69f7d200959f11cde95b568702254111
tonmoy <tonmoy3673@gmail.com>
2024-10-18 15:48:01 +0600

    :tada: implemented show all button with slice

--- src/Components/Home/AllJobs.jsx
+++ src/Components/Home/AllJobs.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import Job from "./job";
 
 const AllJobs=()=>{
-    const [jobs,setJobs]=useState();
+    const [jobs,setJobs]=useState([]);
+    const [jobLength,setJobLength]=useState(6);
     useEffect(()=>{
         fetch('/data/jobs.json')
         .then(res=>res.json())
@@ -18,15 +19,15 @@ const AllJobs=()=>{
             <div className="grid grid-cols-2 lg:grid-cols-3 py-2 lg:py-8 mt-2 lg:mt-6 gap-4 lg:gap-8 items-center justify-center">
             {
                 jobs && jobs.length>0 ? (
-                    jobs.map((job)=><Job job={job} key={job.id}/>)
+                    jobs.slice(0,jobLength).map((job)=><Job job={job} key={job.id}/>)
                 ):(
                     <p>No Jobs Available</p>
                 )
             }
             </div>
             {/* ========== show all Button ============ */}
-            <div>
-                <button className="common-btn hover:bg-amber-500 py-[5px] lg:py-[10px] px-2 lg:px-[15px]">Show All</button>
+            <div className="py-2">
+                <button onClick={()=>setJobLength(jobs.length)} className="common-btn hover:bg-amber-500 py-[5px] lg:py-[10px] px-2 lg:px-[15px]">Show All</button>
             </div>
         </div>
     )
