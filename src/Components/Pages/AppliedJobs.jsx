2686368f811d2ab033f001bb0cad1c025087c7f0
tonmoy <tonmoy3673@gmail.com>
2024-10-24 10:49:16 +0600

    :tada: implemented filter jobs in applied jobs

--- src/Components/Pages/AppliedJobs.jsx
+++ src/Components/Pages/AppliedJobs.jsx
@@ -6,15 +6,31 @@ const AppliedJobs = () => {
   const jobs = useLoaderData();
   console.log(jobs);
   const [appliedJob, setAppliedJob] = useState([]);
+  const [filterJobs,setFilterJobs]=useState([]);
+
+const handleFilter=(filter)=>{
+if (filter === 'all'){
+  setFilterJobs(appliedJob);
+}
+else if (filter === 'remote'){
+  const remoteJob = appliedJob.filter((job)=>job.remote_or_onsite==='Remote');
+  setFilterJobs (remoteJob)
+}
+else if (filter === 'onsite'){
+const onsiteJob= appliedJob.filter((job)=>job.remote_or_onsite==='Onsite');
+setFilterJobs(onsiteJob)
+}
+}
 
   useEffect(() => {
     const getApplied = getJobApplication();
     if (jobs && jobs.length > 0) {
       const applied = jobs.filter((job) => getApplied.includes(job.id));
       setAppliedJob(applied);
+      setFilterJobs(applied);
     }
   }, [jobs]);
-
+console.log(filterJobs);
   return (
     <div>
       <div>
@@ -24,14 +40,25 @@ const AppliedJobs = () => {
       </div>
 
       <div className="flex justify-center">
-        <select className="select select-info w-2/12 max-w-xs">
-          <option selected>All Jobs</option>
-          <option>Remote</option>
-          <option>Onsite</option>
+        <select onChange={(e)=>handleFilter(e.target.value)} className="select select-info w-2/12 max-w-xs">
+          <option value='all' selected>All Jobs</option>
+          <option value='remote'>Remote</option>
+          <option value='onsite'>Onsite</option>
         </select>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-10"></div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-10 gap-6 lg:gap-10">
+        {
+         filterJobs && filterJobs.map((job)=>(
+          <div key={job.id}>
+            <h2> job ID: {job.id}</h2>
+            <h2>Company Name :  {job.company_name}</h2>
+            <h2>Job Title : {job.job_title}</h2>
+            <h2>Job Type: {job.remote_or_onsite}</h2>
+          </div>
+         ))
+        }
+      </div>
     </div>
   );
 };
