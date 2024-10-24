35f43eecebabd68fff03eae63f0c35f3c394d70e
tonmoy <tonmoy3673@gmail.com>
2024-10-24 09:52:56 +0600

    :bulb: display applied jobs count

--- src/Components/Pages/AppliedJobs.jsx
+++ src/Components/Pages/AppliedJobs.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import { getJobApplication } from "../Utility/LocalStorage";
+
 const AppliedJobs =()=>{
-  
+  const jobs= useLoaderData();
+  console.log(jobs);
+  const [appliedJob,setAppliedJob]= useState([]);
+
+  useEffect(()=>{
+    const getApplied = getJobApplication();
+    if (jobs && jobs.length>0) {
+      const applied = jobs.filter((job)=>getApplied.includes(job.id))
+      setAppliedJob(applied);
+    }
+      
+    
+  },[jobs])
+
   return (
     <div>
-
+        <div>
+          <h2 className="text-center text-3xl py-5 lg:py-10">Total Applied Jobs : {appliedJob.length}</h2>
+        </div>
     </div>
   )
 };
